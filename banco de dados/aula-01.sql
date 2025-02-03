b1b5e7f922f3e1801b7426b6b764bcb8bcc65d34
Henry <franzhenry46@gmail.com>
2025-02-03 18:43:33 -0300

    atualizando comentarios e anotaçôes

--- banco de dados/aula-01.sql
+++ banco de dados/aula-01.sql
@@ -1,3 +1,4 @@
+-- -------------------REGRAS-------------------
 -- TODOS OS COMANDO SÂO EM INGLES
 -- TODO COMANDO TERMINA COM PONTO E VIRGULA
 -- TODA TABELA TEM UMA CHAVE PRIMARIA 
@@ -12,14 +13,11 @@ USE sprint1;
 SHOW DATABASES;
 
 -- CRIAR NOSSA PRIMEIRA TABELA 
-
 CREATE TABLE aluno(
 	ra CHAR(8) PRIMARY KEY, -- idAluno INT PRIMARY KEY AUTO_INCREMENT
     nome VARCHAR(30),
     bairro VARCHAR(25)
 );
-
-
 -- COMO REGRA A CHAVE PRIMARIA É SEMPRE DO TIPO INTEIRO (INT)
 -- SE TEM EXCEÇÃO COMO O RA, PODE SER OUTRA TIPAGEM
 
@@ -43,7 +41,7 @@ SELECT * FROM aluno;
 SELECT ra ,nome FROM aluno;
 
 -- EXIBIR OS ALUNOS QUE MORA EM 'Cantiho do Céu'
-
+SELECT * FROM aluno WHERE bairro = 'Cantiho do Céu';
 
 -- EXIBIR OS ALUNOS QUE MORAM NO BAIRRO DIFERENTE 'Cantiho do Céu'
 /* != === <> */
@@ -52,41 +50,35 @@ SELECT * FROM aluno WHERE bairro <> 'Cantiho do Céu';
 
 -- EXIBIR O ALUNO QUE MORA EM FLOR DE MAIO E O ALUNO QUE MORA NO CANTIHO DO CEU 
 SELECT * FROM  aluno WHERE bairro = 'Flor de Maio' OR bairro = 'Cantiho do Céu'; -- PRECISA SER OR NÂO AND(TABELA VERDADE)
-
-
 SELECT * FROM  aluno WHERE bairro IN ('Flor de Maio', 'Cantiho do Céu'); -- PRECISA SER OR NÂO AND(TABELA VERDADE)
 
--- 
 
-SELECT * FROM aluno;
-
--- exibir os nomes que começa com a letra h (COMEÇA)
+-- EXIBIR NOMES DOS ALUNOS QUE COMEÇEM COM A LETRA H(COMEÇA)
 SELECT * FROM aluno WHERE nome LIKE 'H%';
 
--- exibir os nomes que termina com a letra r (TERMINA)
+-- EXIBIR NOMES DOS ALUNOS QUE TERMINA COM A LETRA R(TERMINA)
 SELECT * FROM aluno WHERE nome LIKE '%r';
 
--- exibir os nomes contain a letra y (CONTAIN)
+-- EXIBIR NOMES QUE CONTAIN A LETRA Y (CONTAIN)
 SELECT nome FROM aluno WHERE nome LIKE '%y%';
 
 -- EXIBIR OS NOMES ONDE A SEGUNDA LETRA é E
 SELECT nome FROM aluno WHERE nome LIKE '_e%'; -- underine conta o caracter
 
-SELECT * FROM aluno ;-- underline conta o caracter
-
 -- EXIBIR ALUNOS ORDENADOS EM CRESCENTE PELO NOME 
-
 SELECT * FROM aluno ORDER BY nome ASC;
 
 -- EXIBIR ALUNOS ORDENADOS EM DECRESCENTE PELO NOME 
 SELECT * FROM aluno ORDER BY nome DESC;
 
 -- EXIBIR OS DADOS DOS ALUNOS QUE COMEÇAM H ORDERNADOS PELO BAIRRO
-
 SELECT * FROM aluno WHERE nome LIKE 'H%' ORDER BY bairro ASC; -- ASC POR DEFAULT
 
 -- EXIBIR OS NOMES QUE TEM  2 NOMES
-
 SELECT * FROM aluno WHERE nome LIKE '% %';
 
-SELECT * FROM aluno WHERE bairro LIKE '% % M%'
+-- EXIBIR ALUNOS QUE TECEIRA PALAVRA DO BAIRRO COMEÇE COM M
+SELECT * FROM aluno WHERE bairro LIKE '% % M%';
+
+-- EXCLUIR UMA TABELA 
+DROP TABLE aluno;
