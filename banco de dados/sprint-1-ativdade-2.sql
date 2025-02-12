d0df7edb0e15b4eea6f17afcfdf3c830058fcb2b
HENRY <franzhenry46@gmail.com>
2025-02-11 23:18:50 -0300

    exercicio 03 feito

--- banco de dados/sprint-1-ativdade-2.sql
+++ banco de dados/sprint-1-ativdade-2.sql
@@ -139,4 +139,75 @@ DESCRIBE Musica;
 # l) Limpar os dados da tabela;
 TRUNCATE Musica;
 
+#  ------------------ EXERCICIO 3 ------------------
+
+# No MySQL Workbench, utilizando o banco de dados ‘sprint1’:
+USE sprint1;
+# Criar a tabela chamada Filme para conter os dados: idFilme, título (tamanho 50), genero
+# (tamanho 40), diretor (tamanho 40), sendo que idFilme é a chave primária da tabela.
+# Inserir dados na tabela, procurando colocar um gênero de filme que tenha mais de um
+# filme, e um diretor, que tenha mais de um filme cadastrado. Procure inserir pelo menos
+# uns 7 filmes.
+
+CREATE TABLE Filme(
+	idFilme INT PRIMARY KEY AUTO_INCREMENT,
+  titulo VARCHAR(50),
+  genero VARCHAR(40),
+  diretor VARCHAR(40)
+);  
+
+INSERT INTO Filme VALUES(DEFAULT, 'Agente Oculto', 'Ação', 'Anthony Russo');
+INSERT INTO Filme VALUES(DEFAULT, 'Vingadores: Ultimato', 'Ação', 'Anthony Russo');
+INSERT INTO Filme VALUES(DEFAULT, 'As Aventuras de Pi', 'Aventura', 'Ang Lee');
+INSERT INTO Filme VALUES(DEFAULT, 'Projeto Gemini', 'Ficção científica', 'Ang Lee');
+INSERT INTO Filme VALUES(DEFAULT, 'Jujutsu Kaisen 0', 'Anime', 'Sunghoo Park');
+INSERT INTO Filme VALUES(DEFAULT, 'Your Name', 'Animação', 'Makoto Shinkai');
+INSERT INTO Filme VALUES(DEFAULT, 'Suzume', 'Animação', 'Makoto Shinkai');
+
+# Execute os comandos para:
+# • Exibir todos os dados da tabela.
+SELECT * FROM Filme;
+
+# • Adicionar o campo protagonista do tipo varchar(50) na tabela;
+ALTER TABLE Filme ADD COLUMN protagonista VARCHAR(50);
+
+# • Atualizar o campo protagonista de todas os filmes inseridos;
+UPDATE Filme SET protagonista = 'ciclano' WHERE idFilme= 1;
+UPDATE Filme SET protagonista = 'homen de ferro' WHERE idFilme = 2;
+UPDATE Filme SET protagonista = 'pi' WHERE idFilme = 3;
+UPDATE Filme SET protagonista = 'will smith' WHERE idFilme = 4;
+UPDATE Filme SET protagonista = 'yuta' WHERE idFilme = 5;
+UPDATE Filme SET protagonista = 'deutrano' WHERE idFilme = 6;
+UPDATE Filme SET protagonista = 'Suzume' WHERE idFilme = 7;
+
+# • Modificar o campo diretor do tamanho 40 para o tamanho 150;
+ALTER TABLE Filme MODIFY COLUMN diretor VARCHAR(150);
+
+# • Atualizar o diretor do filme com id=5;
+UPDATE Filme SET diretor = 'gege akutami' WHERE idFilme = 5; 
+
+# • Atualizar o diretor dos filmes com id=2 e com o id=7;
+UPDATE Filme SET diretor = 'adlam sandler' WHERE idFilme IN (2, 7);
+
+# • Atualizar o título do filme com o id=6;
+UPDATE Filme SET titulo = 'Yur name 2' WHERE idFilme = 6; 
+
+# • Excluir o filme com o id=3;
+DELETE FROM Filme WHERE idFilme = 4;
+
+# • Exibir os filmes em que o gênero é diferente de drama;
+SELECT * FROM Filme WHERE genero <> 'drama';
+SELECT * FROM Filme WHERE genero != 'drama';
+
+# • Exibir os dados dos filmes que o gênero é igual ‘suspense’;
+SELECT * FROM Filme WHERE genero = 'suspense'; 
+
+# • Descrever os campos da tabela mostrando a atualização do campo protagonista e
+# diretor;
+DESCRIBE Filme;
+
+# • Limpar os dados da tabela;
+TRUNCATE Filme;
+
+
 
