261a1fe496a4c3633a722922dcfb2aeeda672939
Henry <henry.arcaya@sptech.school>
2025-05-14 20:49:37 -0300

    finalizando atividade em 1 polmodoro

--- banco de dados/aulas/view.sql
+++ banco de dados/aulas/view.sql
@@ -1,16 +1,12 @@
 CREATE DATABASE view_in_mysql;
 DROP DATABASE view_in_mysql;
-
 USE view_in_mysql;
-
-
 
 CREATE TABLE Clientes(
 	cliente_id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(45) NOT NULL,
 	email VARCHAR(255) UNIQUE
 );
-
 
 CREATE TABLE Itens(
 	item_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -40,7 +36,7 @@ CREATE TABLE Pagamento(
 	valor_pago DECIMAL(10,2),
 	data_pagamento DATE,
   CONSTRAINT pk_composta PRIMARY KEY(pagamento_id, pedido_id),
-	CONSTRAINT fk_pagamento_pedido FOREIGN KEY (pedido_id) REFERENCES Pedidos (pedido_id)
+  CONSTRAINT fk_pagamento_pedido FOREIGN KEY (pedido_id) REFERENCES Pedidos (pedido_id)
 );
 
 
@@ -84,8 +80,8 @@ INSERT INTO Pagamento (pagamento_id, pedido_id, valor_pago, data_pagamento) VALU
 # a quantidade de cada item, o total gasto e o pagamento realizado para cada pedido.
 
 SELECT 
-		c.nome AS 'CLiente:', 
-		p.data_pedido AS 'Data do pedido:',
+	c.nome AS 'CLiente:', 
+	p.data_pedido AS 'Data do pedido:',
     p.pedido_id AS 'Código do pedido:',
     i.nome AS 'Pedido:',
     ip.quantidade AS 'Quantidade',
@@ -93,7 +89,7 @@ SELECT
     i.preco * ip.quantidade AS 'Total:',
     pg.valor_pago AS 'Pagamento:',
     pg.data_pagamento AS 'Data do pagamento:'
-    FROM Pedidos AS p 
+FROM Pedidos AS p 
     JOIN Itens_Pedido AS ip 
     ON p.pedido_id = ip.pedido_id 
     JOIN Clientes AS c 
@@ -120,7 +116,7 @@ SELECT
 
 #     Data do pagamento.
 SELECT 
-		c.nome AS 'CLiente', 
+	c.nome AS 'CLiente', 
     i.nome AS 'Pedido',
     ip.quantidade AS 'Quantidade',
     CONCAT('R$ ',FORMAT(i.preco,2)) AS 'Preço',
@@ -128,7 +124,7 @@ SELECT
     SUM(i.preco * ip.quantidade) AS 'Valor total do pedido',
     pg.valor_pago AS 'Pagamento',
     pg.data_pagamento AS 'Data do pagamento'
-    FROM Pedidos AS p 
+FROM Pedidos AS p 
     JOIN Itens_Pedido AS ip 
     ON p.pedido_id = ip.pedido_id 
     JOIN Clientes AS c 
@@ -140,7 +136,7 @@ SELECT
     GROUP BY c.nome, p.pedido_id, pg.valor_pago, pg.data_pagamento, ip.quantidade, i.preco, i.nome;
 
 SELECT 
-		c.nome AS 'CLiente', 
+	c.nome AS 'CLiente', 
     i.nome AS 'Pedido',
     ip.quantidade AS 'Quantidade',
     CONCAT('R$ ',FORMAT(i.preco,2)) AS 'Preço',
@@ -153,7 +149,7 @@ SELECT
 		) AS 'Valor total do pedido',
     pg.valor_pago AS 'Pagamento',
     pg.data_pagamento AS 'Data do pagamento'
-    FROM Pedidos AS p 
+FROM Pedidos AS p 
     JOIN Itens_Pedido AS ip 
     ON p.pedido_id = ip.pedido_id 
     JOIN Clientes AS c 
@@ -190,21 +186,21 @@ CREATE VIEW vw_detalhes_pedido AS SELECT
     ON pg.pedido_id = p.pedido_id 
     GROUP BY c.nome, p.pedido_id, pg.valor_pago, pg.data_pagamento, ip.quantidade, i.preco, i.nome;
   
-  ALTER VIEW vw_detalhes_pedido AS SELECT 
+ALTER VIEW vw_detalhes_pedido AS SELECT 
 		c.nome AS 'CLiente', 
     i.nome AS 'Pedido',
     ip.quantidade AS 'Quantidade',
     CONCAT('R$ ',FORMAT(i.preco,2)) AS 'Preço',
     i.preco * ip.quantidade AS 'Total',
 		(
-  	SELECT SUM(i2.preco * ip2.quantidade)
-  		FROM Itens_Pedido ip2
-  		JOIN Itens i2 ON i2.item_id = ip2.item_id
-  	WHERE ip2.pedido_id = p.pedido_id
+		SELECT SUM(i2.preco * ip2.quantidade)
+			FROM Itens_Pedido ip2
+			JOIN Itens i2 ON i2.item_id = ip2.item_id
+		WHERE ip2.pedido_id = p.pedido_id /*somando todas ocorrencias dos itens do pedido, 1,2,3...*/
 		) AS 'Valor total do pedido',
     pg.valor_pago AS 'Pagamento',
     pg.data_pagamento
-    FROM Pedidos AS p 
+FROM Pedidos AS p 
     JOIN Itens_Pedido AS ip 
     ON p.pedido_id = ip.pedido_id 
     JOIN Clientes AS c 
@@ -216,3 +212,10 @@ CREATE VIEW vw_detalhes_pedido AS SELECT
     GROUP BY c.nome, p.pedido_id, pg.valor_pago, pg.data_pagamento, ip.quantidade, i.preco, i.nome;
     
  SELECT * FROM vw_detalhes_pedido ORDER BY data_pagamento DESC;
+ 
+ /*
+	CREATE VIEW name_view AS query_select
+    ALTER VIEW name_view AS query_select
+    DROP VIEW name_view
+    documentation: https://www.w3schools.com/sql/sql_view.asp
+ */
