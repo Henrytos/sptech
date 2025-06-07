a99220eadbabea0022886b0efd969e13fe9b2750
Henry <henry.arcaya@sptech.school>
2025-06-06 23:37:57 -0300

    algoritmo de ordenação de anime

--- sort-function-recursive.js
+++ sort-function-recursive.js
@@ -1,31 +1,72 @@
 // base case -> is size 0 or 1 element
 // recursive case -> as long as you have subdivision in the array
 
-function sort(numbers) {
-  if (numbers.length <= 1) return numbers;
+function sort(animes) {
+  if (animes.length <= 1) return animes;
 
-  let minorNumbers = [];
-  let majorNumbers = [];
+  let minorAnimesPoints = [];
+  let majorAnimesPoints = [];
 
-  let positionMedium = Math.floor(numbers.length / 2);
-  let comparisonNumber = numbers[positionMedium];
+  let positionMedium = Math.floor(animes.length / 2);
+  let comparisonAnimes = animes[positionMedium];
 
-  for (let position = 0; position < numbers.length; position++) {
-    let number = numbers[position];
+  for (let position = 0; position < animes.length; position++) {
+    let anime = animes[position];
 
     if (position == positionMedium) continue;
 
-    if (number <= comparisonNumber) minorNumbers.push(number);
-    else majorNumbers.push(number);
+    if (anime.points <= comparisonAnimes.points) minorAnimesPoints.push(anime);
+    else majorAnimesPoints.push(anime);
   }
 
-  return [...sort(minorNumbers), comparisonNumber, ...sort(majorNumbers)];
+  return [...sort(minorAnimesPoints), comparisonAnimes, ...sort(majorAnimesPoints)];
 }
 
-const numbers = [4, 1, 2, 32, 3, 4, 5];
-const numbersOrder = sort(numbers);
+const animes = [
+  {
+    title: "Shingeki no Kyojin Season 3 Part 2",
+    image_url: "https://cdn.myanimelist.net/images/anime/1517/100633l.jpg",
+    anime_id: 38524,
+    gender: "Action",
+    target_audience: "Shounen",
+    points: 10
+  },
+  {
+    title: "Gintama. Shirogane no Tamashii-hen - Kouhan-sen",
+    image_url: "https://cdn.myanimelist.net/images/anime/1776/96566l.jpg",
+    anime_id: 37491,
+    gender: "Action",
+    target_audience: "Shounen",
+    points: 102
+  },
+  {
+    title: "Monster",
+    image_url: "https://cdn.myanimelist.net/images/anime/10/18793l.jpg",
+    anime_id: 19,
+    gender: "Drama",
+    target_audience: "Seinen",
+    points: 130
+  },
+  {
+    title: "Gintama.",
+    image_url: "https://cdn.myanimelist.net/images/anime/3/83528l.jpg",
+    anime_id: 34096,
+    gender: "Action",
+    target_audience: "Shounen",
+    points: 210
+  },
+  {
+    title: "Gintama",
+    image_url: "https://cdn.myanimelist.net/images/anime/10/73274l.jpg",
+    anime_id: 918,
+    gender: "Action",
+    target_audience: "Shounen",
+    points: 103
+  }
+];
+const animesOrder = sort(animes);
 
 console.log({
-  numbers,
-  numbersOrder,
+  animes,
+  animesOrder,
 });
